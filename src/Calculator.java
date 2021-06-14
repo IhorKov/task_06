3fa2032f06b49c4bedcba60bb2dd513952cee02b
IhorKov <apasniy1234@gmail.com>
2021-06-14 11:57:53 +0300

    add multiply

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,11 @@
 public class Calculator {
 
     public static void main(String[] args) {
-
+        double a = 2.123;
+        double b = 10.6387;
+        System.out.println(multiply(a, b));
     }
-    public static int sum(int a, int b) {
+    public static double multiply(double a, double b) {
         return a + b;
     }
 }
